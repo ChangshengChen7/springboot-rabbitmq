d1db531796d21dc93d0aeb77e621ed05bf842011
ccsfzx <962186309@qq.com>
2021-10-07 00:11:48 +0800

    masterNew commit

--- src/main/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplication.java
+++ src/main/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplication.java
@@ -13,7 +13,7 @@ public class SpringbootRabbitmqApplication {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
-        System.out.println("4");
+        System.out.println("455master");
     }
 
 }
