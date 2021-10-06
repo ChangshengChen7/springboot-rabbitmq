ed5a6b5000ffc28cb823c33d99b004a481a44c22
ccsfzx <962186309@qq.com>
2021-10-06 22:54:34 +0800

    second commit

--- src/main/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplication.java
+++ src/main/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplication.java
@@ -10,6 +10,7 @@ public class SpringbootRabbitmqApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootRabbitmqApplication.class, args);
+        System.out.println("1");
     }
 
 }
