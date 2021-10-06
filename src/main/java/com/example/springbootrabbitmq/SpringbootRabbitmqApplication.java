d2e42d6585c525000874ff499a69f75a33453edc
fzxwan <67223080+fzxwan@users.noreply.github.com>
2021-10-07 00:44:44 +0800

    Update SpringbootRabbitmqApplication.java

--- src/main/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplication.java
+++ src/main/java/com/example/springbootrabbitmq/SpringbootRabbitmqApplication.java
@@ -15,6 +15,8 @@ public class SpringbootRabbitmqApplication {
         System.out.println("3");
         System.out.println("455master");
         System.out.println("4hotttttttttt5555");
+        System.out.println("pulllllllllll");
+        
     }
 
 }
